dae636de5d741ed7df285887df1bbee659e8ad9e
rt3115@rit.edu <Maggie4182>
2019-05-08 12:30:53 -0400

    to-do

--- prototype/src/gui/GUI.java
+++ prototype/src/gui/GUI.java
@@ -1,4 +1,31 @@
 package gui;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public GUI() {
+        super();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
 }
