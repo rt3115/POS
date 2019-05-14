33d5f5033bf9a6558bffc29aef2c089ab2979128
rt3115@rit.edu <Maggie4182>
2019-05-14 12:31:19 -0400

    new gui is about halfway done when compared to the old one addad all the buttons and regions started to size them correctly edited todo added what functions in register need to be made

--- prototype/src/gui/GUIFinal.java
+++ prototype/src/gui/GUIFinal.java
@@ -1,11 +1,36 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
 
 public class GUIFinal extends Application {
 
-    private String
+    private String genStyle = "" +
+            "-fx-border-style: none;" +
+            "-fx-border-width: 0px;" +
+            "-fx-border-color: black;" +
+            "-fx-border-radius: 0;" +
+            "-fx-background-radius: 0;";
+
+    private String mainBackground = "" +
+            "-fx-background-color: #bed9f4;" +
+            "-fx-padding: 5;" +
+            "-fx-spacing: 10;";
+
+    private String regionStyle = "" +
+            "-fx-background-color: #e4e2e2;" +
+            "-fx-padding: 5;" +
+            "-fx-spacing: 5;" +
+            "-fx-background-insets: 1.5 1.5 1.5 1.5;";
 
     @Override
     public void init() throws Exception {
@@ -20,8 +45,136 @@ public class GUIFinal extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        HBox mainSpace = new HBox();
+        mainSpace.setStyle(mainBackground);
 
+        //Function Col
+        VBox functionsList = new VBox();
+        functionsList.getChildren().addAll(new Button("Function Button"));
 
+        //Register Area Anchor pane
+        GridPane mainGrid = new GridPane();
+
+        //Item View
+        VBox itemViewRegion = new VBox();
+        itemViewRegion.setStyle(regionStyle);
+
+        GridPane itemView = new GridPane();
+        itemView.setStyle("" +
+                "-fx-hgap: 2;" +
+                "-fx-vgap: 2");
+        { //adding some test buttons
+            for(int x = 0; x < 3; x++){
+                for(int y = 0; y < 6; y++){
+                    Button b1 = new Button("Hello");
+                    b1.setStyle(genStyle);
+                    b1.setPrefSize(80, 60);
+                    itemView.add(b1, x ,y);
+                }
+            }
+        }
+
+        HBox adjustSideArea = new HBox();
+        {
+            Button adjustOrderButton = new Button();
+            adjustOrderButton.setStyle(genStyle);
+            adjustOrderButton.setText("Adjust Order");
+            adjustOrderButton.setPrefSize(120, 60);
+
+            Button addSideButton = new Button();
+            addSideButton.setStyle(genStyle);
+            addSideButton.setText("Add Side");
+            addSideButton.setPrefSize(120, 60);
+            adjustSideArea.getChildren().addAll(adjustOrderButton, addSideButton);
+            adjustSideArea.setStyle("" +
+                    "-fx-spacing: 5;");
+        }
+        itemViewRegion.getChildren().addAll(itemView, adjustSideArea);
+
+        //Keypad
+        VBox keyPadRegion = new VBox();
+        keyPadRegion.setStyle(regionStyle);
+        {
+            Label currValue = new Label("123456789");
+            currValue.setPrefWidth(240);
+            currValue.setStyle("" +
+                    "-fx-background-color: white;");
+            currValue.setFont(Font.font(20));
+
+            GridPane keyPadButtons = new GridPane();
+            keyPadButtons.setStyle("" +
+                    "-fx-hgap: 2;" +
+                    "-fx-vgap: 2;");
+            for(int x = 0; x < 3; x++){
+                for(int y = 0; y < 3; y++){
+                    Button b1 = new Button("" + ((x*3) + y + 1));
+                    b1.setStyle(genStyle);
+                    b1.setPrefSize(50 , 50);
+                    keyPadButtons.add(b1, y ,x);
+                }
+            }
+            Button clearButton = new Button("Clear");
+            clearButton.setStyle(genStyle);
+            clearButton.setPrefSize(80, 50);
+            Button addButton = new Button("Add");
+            addButton.setPrefSize(80, 50);
+            addButton.setStyle(genStyle);
+
+            keyPadButtons.add(clearButton, 3, 0);
+            keyPadButtons.add(addButton, 3 , 1);
+
+            Button zeroButton = new Button("0");
+            zeroButton.setStyle(genStyle);
+            zeroButton.setPrefSize(75, 50);
+            Button dotButton = new Button(".");
+            dotButton.setStyle(genStyle);
+            dotButton.setPrefSize(75, 50);
+            HBox keyPadZeroAndDot = new HBox();
+            keyPadZeroAndDot.setStyle("" +
+                    "-fx-spacing: 5");
+            keyPadZeroAndDot.getChildren().addAll(zeroButton, dotButton);
+
+            keyPadRegion.getChildren().addAll(currValue, keyPadButtons, keyPadZeroAndDot);
+        }
+
+        //Trans View
+        VBox transViewRegion = new VBox();
+        transViewRegion.setStyle(regionStyle);
+        {
+            HBox voidRemoveArea = new HBox();
+            Button voidTrans = new Button("Void Trans");
+            voidTrans.setStyle(genStyle);
+            Button removeItem = new Button("Remove Item");
+            removeItem.setStyle(genStyle);
+            voidRemoveArea.getChildren().addAll(voidTrans, removeItem);
+
+            ScrollPane scrollPane = new ScrollPane();
+
+            transViewRegion.getChildren().addAll(voidRemoveArea, scrollPane);
+        }
+
+        //Cash or credit area
+        VBox paymentRegion = new VBox();
+        paymentRegion.setStyle(regionStyle);
+        {
+            Label total = new Label("Total: ");
+            HBox methodsArea = new HBox();
+            Button cashButton = new Button("Cash");
+            Button creditButton = new Button("Credit");
+            methodsArea.getChildren().addAll(cashButton, creditButton);
+
+            paymentRegion.getChildren().addAll(total, methodsArea);
+        }
+
+        //setting the grid
+        mainGrid.add(itemViewRegion, 0, 0);
+        mainGrid.add(keyPadRegion, 0 ,1);
+        mainGrid.add(transViewRegion, 1, 0);
+        mainGrid.add(paymentRegion, 1, 1);
+
+        mainSpace.getChildren().addAll(functionsList, mainGrid);
+        Scene scene = new Scene(mainSpace);
+        stage.setScene(scene);
         stage.show();
     }
 
