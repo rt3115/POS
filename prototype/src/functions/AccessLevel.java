d89be28b36f9f2e475062f49ebca7704851ccab4
rt3115@rit.edu <Maggie4182>
2019-05-14 12:54:44 -0400

    added the function abstract class added the AccessLevel enum added things to TO-DO

---
+++ prototype/src/functions/AccessLevel.java
@@ -0,0 +1,10 @@
+package functions;
+
+public enum AccessLevel {
+
+    NORMAL, //regular ussage
+    MANAGER, //manager level functions
+    OWNER, //owner level control
+    ROOT; //root level, sees all debug
+
+}
---
+++ prototype/src/functions/Function.java
@@ -0,0 +1,10 @@
+package functions;
+
+public abstract class Function {
+
+
+    public abstract String getName();
+
+    public abstract void execute();
+
+}
