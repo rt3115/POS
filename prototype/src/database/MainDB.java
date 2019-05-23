5a798aae6ce7147f007cc552512b17daba8518cd
jre8851 <jre8851@rit.edu>
2019-05-23 19:10:18 -0400

    tried both getters, but I don't think I know what I'm doing. But I should commit in case I pass out

--- prototype/src/database/MainDB.java
+++ prototype/src/database/MainDB.java
@@ -1,4 +1,41 @@
 package database;
 
+import common.Topping;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainDB {
+    public List<Topping> toppings = new LinkedList<>();
+    public MainDB(){
+        toppings.add(new Topping("HomeFries", "HomeFries", .00));
+        toppings.add(new Topping("Mac Salad","Mac Salad", .00));
+        toppings.add(new Topping("Hot Sauce", "Hot Sauce", .00, .50, true, true));
+        toppings.add(new Topping("Lettuce" , "Lettuce",  .00));
+        toppings.add(new Topping("Tomato", "Tomato", .00));
+        toppings.add(new Topping("Onions", "Onions",.00));
+        toppings.add(new Topping("Cheese", "Cheese",.00, .50));
+    }
+    public List<Topping> getTopping(){
+        //i need to read in the database? or load it in or something?
+        return toppings;
+    }
+    public void getFood(){//cant have food List?
+
+    }
+
 }
+//list of foods, can be anything list array list costom?
+//list of toppings list of sides. sides and toppings are same object
+//grab list of everything.
+//how store? may only need one class
+//add food, add topping. toppings vs sides.
+//needs to be able to editItem- one function has to check one list.
+
+//list food, topping, side, everything getlists
+//addItem/Addfoodtopping- just adds to list
+//editeItem/Editfoodtopping
+//constructor, can have start
+//start read in file database
+//periodically save
+//load, reload by request. changes made outside of program, csv-c  can open files in excel
